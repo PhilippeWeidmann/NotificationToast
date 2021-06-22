6df6ff769862e92d9f21ca7fc45fd597a74f1f19
Ivan Sapozhnik <ivan.sapozhnik@sixt.com>
2021-06-22 21:21:20 +0200

    Various enhancements

--- Sources/NotificationToast/ToastView.swift
+++ Sources/NotificationToast/ToastView.swift
@@ -9,6 +9,11 @@ import UIKit
 import QuartzCore
 
 public class ToastView: UIView {
+    public enum TextAlignment {
+        case left
+        case center
+        case right
+    }
 
     public override var bounds: CGRect {
         didSet {
@@ -16,10 +21,31 @@ public class ToastView: UIView {
         }
     }
 
-    private let height: CGFloat = 50
-    private var hStack: UIStackView
-    private let darkBackgroundColor = UIColor(red: 0.13, green: 0.13, blue: 0.13, alpha: 1.00)
-    private let lightBackgroundColor = UIColor(red: 0.99, green: 0.99, blue: 0.99, alpha: 1.00)
+    private let initialTransform = CGAffineTransform(translationX: 0, y: -100)
+    private let hStack: UIStackView = {
+        let stackView = UIStackView()
+        stackView.alignment = .center
+        return stackView
+    }()
+    
+    private lazy var vStack: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.alignment = vStackAlignment
+        return stackView
+    }()
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 1
+        if #available(iOS 13.0, *) {
+            label.textColor = .label
+        } else {
+            label.textColor = .black
+        }
+        return label
+    }()
+    
     private var viewBackgroundColor: UIColor? {
         if #available(iOS 12.0, *) {
             return traitCollection.userInterfaceStyle == .dark ? darkBackgroundColor : lightBackgroundColor
@@ -27,40 +53,86 @@ public class ToastView: UIView {
             return lightBackgroundColor
         }
     }
+    
+    private var vStackAlignment: UIStackView.Alignment {
+        switch textAlignment {
+        case .left:
+            return .leading
+        case .center:
+            return .center
+        case .right:
+            return .trailing
+        }
+    }
+    
     private var onTap: (() -> ())?
 
     /// Hide the view automatically after showing ?
     public var autoHide = true
+    
     /// Display time for the notification view in seconds
-    public var displayTime: TimeInterval = 1
+    public var displayTime: TimeInterval = 3
+    
+    /// Appearence animation duration
+    public var showAnimationDuration = 0.3
+    
+    /// Disappearence animation duration
+    public var hideAnimationDuration = 0.3
+    
     /// Hide the view automatically on tap ?
     public var hideOnTap = true
+    
+    /// Title and subtitle text alignment
+    public var textAlignment: TextAlignment = .center {
+        didSet {
+            vStack.alignment = vStackAlignment
+        }
+    }
+    
+    /// Title text color
+    public var titleTextColor: UIColor = .black {
+        didSet {
+            titleLabel.textColor = titleTextColor
+        }
+    }
+    
+    /// Background color in dark mode
+    public var darkBackgroundColor = UIColor(red: 0.13, green: 0.13, blue: 0.13, alpha: 1.00) {
+        didSet {
+            backgroundColor = viewBackgroundColor
+        }
+    }
+
+    /// Background color in normal mode
+    public var lightBackgroundColor = UIColor(red: 0.99, green: 0.99, blue: 0.99, alpha: 1.00) {
+        didSet {
+            backgroundColor = viewBackgroundColor
+        }
+    }
 
     public init(title: String, titleFont: UIFont = .systemFont(ofSize: 13, weight: .regular),
                 subtitle: String? = nil, subtitleFont: UIFont = .systemFont(ofSize: 11, weight: .light),
                 icon: UIImage? = nil, iconSpacing: CGFloat = 16, onTap: (() -> ())? = nil) {
-        hStack = UIStackView(frame: CGRect.zero)
-        super.init(frame: CGRect.zero)
+        super.init(frame: .zero)
 
         backgroundColor = viewBackgroundColor
 
         getTopViewController()?.view.addSubview(self)
+        
         hStack.spacing = iconSpacing
-        hStack.axis = .horizontal
-        hStack.alignment = .center
 
-        let vStack = UIStackView(frame: CGRect.zero)
-        vStack.axis = .vertical
-        vStack.alignment = .center
-
-        let titleLabel = UILabel(frame: CGRect.zero)
-        titleLabel.numberOfLines = 1
         titleLabel.font = titleFont
         titleLabel.text = title
         vStack.addArrangedSubview(titleLabel)
 
         if let icon = icon {
-            let iconImageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 28, height: 28))
+            let iconImageView = UIImageView()
+            iconImageView.contentMode = .scaleAspectFit
+            iconImageView.translatesAutoresizingMaskIntoConstraints = false
+            NSLayoutConstraint.activate([
+                iconImageView.widthAnchor.constraint(equalToConstant: 28),
+                iconImageView.heightAnchor.constraint(equalToConstant: 28)
+            ])
             if #available(iOS 13.0, *) {
                 iconImageView.tintColor = .label
             } else {
@@ -71,13 +143,13 @@ public class ToastView: UIView {
         }
 
         if let subtitle = subtitle {
-            let subtitleLabel = UILabel(frame: CGRect.zero)
+            let subtitleLabel = UILabel()
             if #available(iOS 13.0, *) {
                 subtitleLabel.textColor = .secondaryLabel
             } else {
                 subtitleLabel.textColor = .lightGray
             }
-            subtitleLabel.numberOfLines = 1
+            subtitleLabel.numberOfLines = 0
             subtitleLabel.font = subtitleFont
             subtitleLabel.text = subtitle
             vStack.addArrangedSubview(subtitleLabel)
@@ -93,7 +165,13 @@ public class ToastView: UIView {
         setupConstraints()
         setupStackViewConstraints()
 
-        transform = CGAffineTransform(translationX: 0, y: -100)
+        transform = initialTransform
+        clipsToBounds = true
+    }
+    
+    public override func layoutSubviews() {
+        super.layoutSubviews()
+        layer.cornerRadius = bounds.height / 2
     }
 
     @available(iOS 10.0, *)
@@ -105,21 +183,21 @@ public class ToastView: UIView {
     }
 
     public func show() {
-        UIView.animate(withDuration: 0.4, delay: 0.0, options: .curveEaseOut, animations: {
+        UIView.animate(withDuration: showAnimationDuration, delay: 0.0, options: .curveEaseOut, animations: {
             self.transform = .identity
-        }) { [self] (completed) in
+        }) { [self] _ in
             if autoHide {
                 hide(after: displayTime)
             }
         }
     }
 
-    public func hide(after time: TimeInterval) {
+    public func hide(after time: TimeInterval = 0.0) {
         DispatchQueue.main.asyncAfter(deadline: .now() + time, execute: {
-            UIView.animate(withDuration: 0.2, delay: 0, options: .curveEaseOut, animations: {
-                self.transform = CGAffineTransform(translationX: 0, y: -100)
-            }) { (completed) in
-                self.removeFromSuperview()
+            UIView.animate(withDuration: self.hideAnimationDuration, delay: 0, options: .curveEaseIn, animations: { [self] in
+                transform = initialTransform
+            }) { [self] _ in
+                removeFromSuperview()
             }
         })
     }
@@ -143,28 +221,26 @@ public class ToastView: UIView {
     }
 
     private func setupConstraints() {
+        guard let superview = superview else { return }
         translatesAutoresizingMaskIntoConstraints = false
-        let heightConstraint = NSLayoutConstraint(item: self, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: height)
-
-        let centerConstraint = NSLayoutConstraint(item: self, attribute: .centerX, relatedBy: .equal, toItem: superview, attribute: .centerX, multiplier: 1, constant: 0)
-        let topConstraint = NSLayoutConstraint(item: self, attribute: .top, relatedBy: .equal, toItem: superview, attribute: .topMargin, multiplier: 1, constant: 0)
-
-        let leadingConstraint = NSLayoutConstraint(item: self, attribute: .leading, relatedBy: .greaterThanOrEqual, toItem: superview, attribute: .leadingMargin, multiplier: 1, constant: 8)
-        let trailingConstraint = NSLayoutConstraint(item: self, attribute: .trailing, relatedBy: .lessThanOrEqual, toItem: superview, attribute: .trailingMargin, multiplier: 1, constant: -8)
-        clipsToBounds = true
-        layer.cornerRadius = height / 2
-        superview?.addConstraints([heightConstraint, leadingConstraint, trailingConstraint, centerConstraint, topConstraint])
+        NSLayoutConstraint.activate([
+            centerXAnchor.constraint(equalTo: superview.centerXAnchor),
+            topAnchor.constraint(equalTo: superview.layoutMarginsGuide.topAnchor, constant: 8),
+            bottomAnchor.constraint(lessThanOrEqualTo: superview.layoutMarginsGuide.bottomAnchor, constant: -8),
+            leadingAnchor.constraint(greaterThanOrEqualTo: superview.leadingAnchor, constant: 8),
+            trailingAnchor.constraint(lessThanOrEqualTo: superview.trailingAnchor, constant: -8)
+        ])
     }
 
     private func setupStackViewConstraints() {
         hStack.translatesAutoresizingMaskIntoConstraints = false
-
-        let leadingConstraint = NSLayoutConstraint(item: hStack, attribute: .leading, relatedBy: .equal, toItem: self, attribute: .leading, multiplier: 1, constant: 24)
-        let trailingConstraint = NSLayoutConstraint(item: hStack, attribute: .trailing, relatedBy: .equal, toItem: self, attribute: .trailing, multiplier: 1, constant: -24)
-        let topConstraint = NSLayoutConstraint(item: hStack, attribute: .top, relatedBy: .equal, toItem: self, attribute: .top, multiplier: 1, constant: 0)
-        let bottomConstraint = NSLayoutConstraint(item: hStack, attribute: .bottom, relatedBy: .equal, toItem: self, attribute: .bottom, multiplier: 1, constant: 0)
-
-        addConstraints([leadingConstraint, trailingConstraint, topConstraint, bottomConstraint])
+        
+        NSLayoutConstraint.activate([
+            hStack.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 24),
+            hStack.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -24),
+            hStack.topAnchor.constraint(equalTo: self.topAnchor, constant: 8),
+            hStack.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -8)
+        ])
     }
 
     private func setupShadow() {
@@ -177,7 +253,7 @@ public class ToastView: UIView {
 
     @objc private func didTap() {
         if hideOnTap {
-            hide(after: 0)
+            hide()
         }
         onTap?()
     }
@@ -185,5 +261,4 @@ public class ToastView: UIView {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
 }
